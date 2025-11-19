1b5b3446ec3eaad06c300407c30ed8fa5d2e1e33
Paco Gimeno <pacogim.pro@pm.me>
2025-11-19 13:36:54 +0100

    Added text size selector

--- Paco/ssr_spa/srcs/public/modules/options.ts
+++ Paco/ssr_spa/srcs/public/modules/options.ts
@@ -2,6 +2,7 @@ const langs = ['En', 'Fr', 'Es']
 
 function updateLanguage(origin: 'init' | 'event') {
 	const $languageOption = document.querySelector('span[data-action="updateLanguage"]') as HTMLSpanElement
+	if (!$languageOption) return
 	const currentOption: number | undefined = Number($languageOption?.dataset?.currentoption)
 	let lang
 
@@ -13,10 +14,26 @@ function updateLanguage(origin: 'init' | 'event') {
 	localStorage.setItem('lang', lang)
 }
 
+function updateTextSize(origin: 'init' | 'event') {
+	const $textSize = document.querySelector('span[data-action="updateTextSize"]') as HTMLSpanElement
+	if (!$textSize) return
+	let textSize: string | undefined = $textSize?.dataset?.currentoption
+
+	if (origin === 'init') {
+		textSize = localStorage.getItem('textSize') || '16'
+		$textSize.dataset.currentoption = textSize
+	}
+	$textSize.innerText = `Text Size (${textSize}px)`
+
+	document.documentElement.style.setProperty('--text-size', `${textSize}px`)
+	localStorage.setItem('textSize', textSize)
+}
+
 function handleOptionClick(evt: any) {
 	let dataset = evt.target.dataset
 
 	if (dataset.action === 'updateLanguage') updateLanguage('event')
+	if (dataset.action === 'updateTextSize') updateTextSize('event')
 }
 
 export function initOptionEvents() {
@@ -24,6 +41,7 @@ export function initOptionEvents() {
 		$el.addEventListener('click', handleOptionClick)
 	})
 	updateLanguage('init')
+	updateTextSize('init')
 }
 
 export function cleanOptionEvents() {
