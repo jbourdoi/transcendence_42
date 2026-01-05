8372883b1e19948d1437200529f15c78ba779986
Christella De Sousa <de_sousa_christella@outlook.com>
2026-01-05 11:03:02 +0100

    fix(login): get value from parsing jwk object

--- services/server/srcs/backend/crud/jwt.crud.ts
+++ services/server/srcs/backend/crud/jwt.crud.ts
@@ -4,7 +4,7 @@ import { log } from '../logs.js';
 
 export async function getJwsSecret() {
     const jwkObject = JSON.parse(await getSecret('jws_secret'));
-    const jwk: JWK = JSON.parse(jwkObject.message);
+    const jwk: JWK = JSON.parse(jwkObject.message.value);
     const secretKey: CryptoKey = await importJWK(jwk, 'HS256') as CryptoKey;
     return secretKey;
 }
@@ -12,7 +12,7 @@ export async function getJwsSecret() {
 
 export async function getJweSecret() {
     const jwkObject = JSON.parse(await getSecret('jwe_secret'));
-    const jwk: JWK = JSON.parse(jwkObject.message);
+    const jwk: JWK = JSON.parse(jwkObject.message.value);
     const secretKey: CryptoKey = await importJWK(jwk, 'A256GCM') as CryptoKey;
     return secretKey;
 }
