bad1af555f14b13498e7fbe1fd35f10f3afaf0b2
Christella De Sousa <de_sousa_christella@outlook.com>
2026-01-15 14:39:42 +0100

    refactor(.env): Add client_id and client_secret to setup_project script

--- scripts/setup_project.sh
+++ scripts/setup_project.sh
@@ -8,6 +8,8 @@ echo "Generating .env file from .env.tpl"
 ENV=".env"
 cp .env.tpl $ENV
 
+CLIENT_ID="u-s4t2ud-9f30b2430e51c381ae5e38158295eef89230a74b070231a798bd1bcb7a01709c"
+CLIENT_SECRET="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"
 PASSPHRASE="abcde"
 LOGS_PATH="./logs"
 GF_ADMIN_USER="adminuser"
@@ -26,6 +28,8 @@ else
 fi
 
 echo "Filling in the placeholders in .env file"
+$SED "s|^\(CLIENT_ID=\).*|\1${CLIENT_ID}|" $ENV
+$SED "s|^\(CLIENT_SECRET=\).*|\1${CLIENT_SECRET}|" $ENV
 $SED "s|^\(PASSPHRASE=\).*|\1${PASSPHRASE}|" $ENV
 $SED "s|^\(LOGS_PATH=\).*|\1${LOGS_PATH}|" $ENV
 $SED "s|^\(GF_ADMIN_USER=\).*|\1${GF_ADMIN_USER}|" $ENV
