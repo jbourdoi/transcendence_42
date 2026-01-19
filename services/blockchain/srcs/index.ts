e470d4c48fc49fc6bb3ad706174adbe924b03275
Paco Gimeno <pacogim.pro@pm.me>
2026-01-19 09:51:37 +0100

    clean(blockchain): remove all useless files

--- services/blockchain/srcs/index.ts
+++
@@ -1,8 +0,0 @@
-import { routes } from './router'
-import { BunRequest } from 'bun'
-
-Bun.serve({
-	port: 3001,
-	routes,
-	fetch: () => new Response('Page not found', { status: 404 })
-})
--- services/blockchain/srcs/types/route.type.ts
+++
@@ -1,5 +0,0 @@
-export type RouteType = {
-	method: 'GET' | 'POST' | 'DELETE' | 'PUT'
-	pathName: string
-	handler: (params?: URLSearchParams) => Response
-}
