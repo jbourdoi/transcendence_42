4c9f03093e2883007afb3bb8da986d216ee1d30d
Christella De Sousa <de_sousa_christella@outlook.com>
2026-01-20 10:14:35 +0100

    feat(chat): send msg to only non-blocked users

--- services/chat/srcs/channels/global.channel.ts
+++ services/chat/srcs/channels/global.channel.ts
@@ -1,11 +1,30 @@
+import { dbPostQuery } from '../services/db.service'
 import { clientsSocket } from '../state/clients.state'
 import { BunSocketType } from '../types/bunSocket.type'
 
-export function globalChannel(ws: BunSocketType, message: string | Buffer<ArrayBuffer>) {
-	// TODO Get user blocked list and don't send message to socket
-	// ws.data.username Proprio de la websocket
+function isClientBlocked(blockedClients: { blocked_username: string }[], username: string): boolean {
+	const isBlocked = blockedClients.findIndex(c => c.blocked_username === username) !== -1
+	console.log(`Is ${username} blocked: `, isBlocked)
+	return isBlocked
+}
+
+export async function globalChannel(ws: BunSocketType, message: string | Buffer<ArrayBuffer>) {
+	const res = await dbPostQuery({
+		endpoint: 'dbAll',
+		query: {
+			verb: 'SELECT',
+			sql: 'SELECT blocked_username FROM blocks WHERE blocker_username = ?',
+			data: [ws.data.username]
+		}
+	})
+	if (res.status >= 400) {
+		console.log('status: ', res.status, 'message: ', res.message)
+		return
+	}
+	const clients = res.data as { blocked_username: string }[]
+	console.log('Clients blocked: ', clients)
 	for (const client of clientsSocket) {
-		if (client.readyState === WebSocket.OPEN) {
+		if (client.readyState === WebSocket.OPEN && !isClientBlocked(clients, client.data.username)) {
 			client.send(message)
 		}
 	}
