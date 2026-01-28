4a19c737d1e5d930b54cbed2d353f98ae4674295
Paco Gimeno <pgimeno@student.42.fr>
2026-01-28 09:17:47 +0100

    chore(setup): add new secret

--- scripts/setup_project.sh
+++ scripts/setup_project.sh
@@ -16,7 +16,7 @@ ENV=".env"
 cp .env.tpl $ENV
 
 CLIENT_ID="u-s4t2ud-9f30b2430e51c381ae5e38158295eef89230a74b070231a798bd1bcb7a01709c"
-CLIENT_SECRET="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"
+CLIENT_SECRET="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"
 GMAIL_USER="transcendencebunbun@gmail.com"
 GMAIL_PWD="ntpatodbxrciyink"
 VAULT_UNSEAL_PASSPHRASE="abcde"
